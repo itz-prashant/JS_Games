0148451fd19581237ed603d7c312ba744fb373c5
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-12 18:29:44 +0530

    Add chess board

--- Chess/render/main.js
+++ Chess/render/main.js
@@ -1,7 +1,15 @@
+const rootDiv = document.getElementById('root')
+
 function initGameRender(data) {
     data.forEach(element => {
+        const rowElm = document.createElement('div')
+        rowElm.classList.add('rowelm')
+        rootDiv.appendChild(rowElm)
+
         element.forEach(square => {
-            console.log(square);
+            const squareElm = document.createElement('div')
+            squareElm.classList.add(square.color, "square")
+            rowElm.appendChild(squareElm)
         })
     });
 }
