117d1e6a18fc9522bfa7f969baa9c17467d7c2a8
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-21 14:46:46 +0530

    Add all pieces in board when game start

--- Chess/data/pieces.js
+++ Chess/data/pieces.js
@@ -1,17 +1,100 @@
-function blackPawn(currentPosition){
-    
-    return {
-        currentPosition,
-        img: "Assets/black/pawn.png"
-    }
+// Black Piece
+
+function blackPawn(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/black/pawn.png",
+  };
 }
 
-function whitePawn(currentPosition){
-    
-    return {
-        currentPosition,
-        img: "Assets/white/pawn.png"
-    }
+function blackRook(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/black/rook.png",
+  };
 }
 
-export {blackPawn,whitePawn};
+function blackKnight(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/black/knight.png",
+  };
+}
+
+function blackBishop(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/black/bishop.png",
+  };
+}
+function blackking(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/black/king.png",
+  };
+}
+
+function blackQueen(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/black/queen.png",
+  };
+}
+
+// White Piece
+
+function whitePawn(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/white/pawn.png",
+  };
+}
+
+function whiteRook(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/white/rook.png",
+  };
+}
+
+function whiteKnight(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/white/knight.png",
+  };
+}
+
+function whiteBishop(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/white/bishop.png",
+  };
+}
+function whiteking(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/white/king.png",
+  };
+}
+
+function whiteQueen(currentPosition) {
+  return {
+    currentPosition,
+    img: "Assets/white/queen.png",
+  };
+}
+
+export {
+  blackPawn,
+  blackRook,
+  blackBishop,
+  blackKnight,
+  blackking,
+  blackQueen,
+  whitePawn,
+  whiteRook,
+  whiteKnight,
+  whiteBishop,
+  whiteking,
+  whiteQueen,
+};
